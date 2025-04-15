ff47e5ed3f89fa0097120959f6ba72660ca1812a
António Cunha <tojo98@live.com.pt>
2025-04-15 23:22:02 +0100

    userService and userController working with register and login. Password already encrypted by argon2, just have to add jwt

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const routes = require("./router/routes");
+const productRoutes = require("./router/productRoutes");
+const userRoutes = require("./router/userRoutes");
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
-app.use(routes);
+app.use(productRoutes, userRoutes)
 
 app.listen(port, function() {
     console.log(`Server started on port ${port}`);
