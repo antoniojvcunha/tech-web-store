53a1a11618d1400e94baa6364370895a09d9d455
António Cunha <tojo98@live.com.pt>
2025-04-28 23:42:49 +0100

    add final logo add best seller grid to the homepage

--- frontEnd/frontEnd-webStore/src/components/Navbar.jsx
+++ frontEnd/frontEnd-webStore/src/components/Navbar.jsx
@@ -8,13 +8,10 @@ function Navbar() {
         {/* Logo */}
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="../public/images/logo.png"
             className="h-8"
             alt="Flowbite Logo"
           />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            TechShop
-          </span>
         </Link>
 
         {/* Botão mobile */}
