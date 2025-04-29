73eb11dab11e2037daeb03db5fda23dac4c96c84
António Cunha <tojo98@live.com.pt>
2025-04-29 23:26:10 +0100

    working one about us page

--- frontEnd/frontEnd-webStore/src/App.jsx
+++ frontEnd/frontEnd-webStore/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from "wouter";
 import HomeView from "./views/HomeView";
+import AboutUs from "./views/AboutUs";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <Switch>
           <Route path="/" component={HomeView}></Route>
+          <Route path="/aboutus" component={AboutUs}></Route>
         </Switch>
       </div>
     </>
